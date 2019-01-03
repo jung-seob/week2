3bbe59f910d29c35a37b3976ad82aa451985199d
niley7464 <jyl7464@gmail.com>
2019-01-03 16:00:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/q/week2/Tab2.java
@@ -0,0 +1,19 @@
+package com.example.q.week2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2 extends Fragment {
+    View rootView;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.tab2,container,false);
+        return rootView;
+    }
+}
